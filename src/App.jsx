e362d9bd7deb449dbaf90a2995521f85db7266b9
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-12-05 15:09:19 -0500

    refactor: create table of coins information

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignUpPage />} />
             <Route path="/account" element={<AccountPage />} />
-          </Routes>{" "}
+          </Routes>
         </>
       )}
     </ThemeProvider>
--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,5 +1,61 @@
-const CoinSearch = () => {
-  return <div>CoinSearch</div>;
+import { AiOutlineStar } from "react-icons/ai";
+
+/* eslint-disable react/prop-types */
+const CoinSearch = ({ coins }) => {
+  return (
+    // Searching Coins
+    <div>
+      <div>
+        <h1>Search Crypto</h1>
+        <form>
+          <input type="text" placeholder="Search a coin..." />
+        </form>
+      </div>
+
+      {/* Header Table */}
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>24h Volume</th>
+            <th>Market</th>
+            <th>Last 7 Days</th>
+          </tr>
+        </thead>
+
+        {/* Coins Info Table */}
+        <tbody>
+          {coins.map((coin) => (
+            <tr key={coin.id}>
+              <td>
+                <AiOutlineStar />
+              </td>
+              <td>{coin?.market_cap_rank}</td>
+              <td>
+                <div>
+                  {" "}
+                  <img src={coin.image} alt={coin.id} />
+                  <p>{coin.name}</p>
+                </div>
+              </td>
+
+              <td>{coin.symbol}</td>
+              <td>{coin.current_price}</td>
+              <td>{coin.price_change_percentage_24h}</td>
+              <td>{coin.total_volume}</td>
+              <td>{coin.market_cap}</td>
+              <td>{coin.sparkline_in_7d.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default CoinSearch;

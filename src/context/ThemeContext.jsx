5c9b65fdf4fecde2f088dc0326856b7baa91bf24
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-12-04 20:24:22 -0500

    feat: create theme context

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useEffect, useState } from "react";
+
+const getInitialTheme = () => {
+  if (typeof window !== "undefined" && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem("color-theme");
+    if (typeof storedPrefs === "string") {
+      return storedPrefs;
+    }
+
+    // if local storage doesn't contain a theme preference
+    // => check user's system preference:
+    const userMedia = window.matchMedia("(prefers-color-scheme: dark)");
+    if (userMedia.matches) {
+      return "dark";
+    }
+  }
+  return "light";
+};
+
+// ================== THEME MANAGEMENT ==================
+
+export const ThemeContext = createContext();
+export const ThemeProvider = ({ initialTheme, children }) => {
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  const rawSetTheme = (theme) => {
+    const root = window.document.documentElement;
+    const isDark = theme === "dark";
+
+    root.classList.remove(isDark ? "light" : "dark");
+    root.classList.add(theme);
+
+    // save theme to user's local storage
+    localStorage.setItem("color-theme", theme);
+  };
+
+  if (initialTheme) {
+    rawSetTheme(initialTheme);
+  }
+
+  useEffect(() => {
+    rawSetTheme(theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};

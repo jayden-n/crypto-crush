30ceb695965f2d116f965ba12949663e668e6ac4
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-12-07 21:43:27 -0500

    refactor: implement debounce when searching coins for better UX

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,16 +1,27 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import CoinItem from "./CoinItem";
+import { debounce } from "lodash";
 
-/* eslint-disable react/prop-types */
 const CoinSearch = ({ coins }) => {
   const [searchText, setSearchText] = useState("");
+  const [filteredCoins, setFilteredCoins] = useState(coins);
+
+  const delayedSearch = debounce((text) => {
+    // filtered coins
+    const filtered = coins.filter((coin) =>
+      coin.name.toLowerCase().includes(text.toLowerCase()),
+    );
+    setFilteredCoins(filtered);
+  }, 500);
 
   const handleSearchText = (e) => {
-    setSearchText(e.target.value);
+    const inputText = e.target.value.trim();
+    setSearchText(inputText);
+    delayedSearch(inputText);
   };
 
   return (
-    // Searching Coins
     <div className="rounded-div my-4">
       <div className="flex flex-col justify-between pb-6 pt-4 text-center md:flex-row md:text-right">
         <h1 className="my-2 text-xl font-bold">Search Crypto</h1>
@@ -24,38 +35,25 @@ const CoinSearch = ({ coins }) => {
         </form>
       </div>
 
-      {/* Header Table */}
-      <table className=" w-full border-collapse text-center text-sm">
+      <table className="w-full border-collapse text-center text-sm">
         <thead>
           <tr className="h-[50px] border-b">
             <th></th>
-            <th className=" px-4">#</th>
-            <th className=" text-left">Coin</th>
+            <th className="px-4">#</th>
+            <th className="text-left">Coin</th>
             <th>Symbol</th>
             <th>Price</th>
             <th>24h</th>
-            <th className=" mr-2 hidden md:table-cell">24h Volume</th>
-            <th className=" hidden sm:table-cell">Market</th>
+            <th className="mr-2 hidden md:table-cell">24h Volume</th>
+            <th className="hidden sm:table-cell">Market</th>
             <th>Last 7 Days</th>
           </tr>
         </thead>
 
-        {/* Coins Info Table */}
         <tbody>
-          {coins
-            .filter((value) => {
-              /* filtering coins by lowercase names */
-              if (searchText === "") {
-                return value;
-              } else if (
-                value.name.toLowerCase().includes(searchText.toLowerCase())
-              ) {
-                return value;
-              }
-            })
-            .map((coin) => (
-              <CoinItem coin={coin} key={coin.id} />
-            ))}
+          {filteredCoins.map((coin) => (
+            <CoinItem coin={coin} key={coin.id} />
+          ))}
         </tbody>
       </table>
     </div>
